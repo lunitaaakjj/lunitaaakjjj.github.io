7348ca8d5b64a67195c3afed1a7a90bce85d3b3f
luna <lunaazulfigola@gmail.com>
2022-11-22 15:05:31 -0300

    para mi novio

---
+++ index.js
@@ -0,0 +1,12 @@
+const noBtn = document.querySelector('#noBtn');
+
+noBtn.addEventListener('mouseover', function () {
+    const randomX = parseInt(Math.random()*100);
+    const randomY = parseInt(Math.random()*100);
+    noBtn.style.setProperty('top',randomY+'%');
+    noBtn.style.setProperty('left',randomX+'%');
+    noBtn.style.setProperty('transform',`translate(-${randomX}%,-${randomY}%)`);
+})
+function redireccion(){
+    location.href = "http://127.0.0.1:5500/pag%202/ventana.html"
+}
